4a2f53213172fd75b8f40dc880d0757ae716a44d
Pablo <a20203651@pucp.edu.pe>
2023-08-24 20:43:32 -0500

    Creando archivo

---
+++ LAB1_GTICS_20203651/src/main/java/com/example/lab1_gtics_20203651/Lab1Gtics20203651Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20203651;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Gtics20203651Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Gtics20203651Application.class, args);
+    }
+
+}
---
+++ LAB1_GTICS_20203651/src/test/java/com/example/lab1_gtics_20203651/Lab1Gtics20203651ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20203651;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Gtics20203651ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
